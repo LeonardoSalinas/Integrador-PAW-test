8b5ea0d1fa4418f613e37ce98ec07688de5a9cf1
LeonardoSalinas <sal_chueco@hotmail.com>
2018-08-02 21:43:11 -0300

    Update index.php

--- view/index.php
+++ view/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Inicio - Inmobiliaria Mercedes</title>
+	<title>Home - Inmobiliaria Mercedes</title>
 	<link rel="stylesheet" id="styleLink" href="styles/style1.css">
 </head>
 
